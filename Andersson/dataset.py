a97cb6885bc3c1d3daf956532b3d07b1cc25594d
pezosanta <pezosanta@gmail.com>
2021-02-14 21:39:50 +0100

    loading dataset (faster, more memory efficiently) into 4 chunks

--- Andersson/dataset.py
+++ Andersson/dataset.py
@@ -1,5 +1,6 @@
 import torch
 from torch.utils.data import Dataset, DataLoader
+from natsort import natsorted
 import glob
 import json
 import numpy as np
@@ -14,6 +15,10 @@ test_database_path = database_path + "test/"
 train_json_path = glob.glob(train_database_path + "*.json")
 val_json_path = glob.glob(val_database_path + "*.json")
 test_json_path = glob.glob(test_database_path + "*.json")
+
+train_json_path = natsorted(train_json_path)#[0:54]
+val_json_path = natsorted(val_json_path)#[0:18]
+test_json_path = natsorted(test_json_path)
 
 
 
@@ -37,13 +42,15 @@ class Andersson_dataset(Dataset):
 
         self._create_dataset()
 
+        self.length = self._get_length()
+
     
 
     def _create_dataset(self):
-        #print(len(self.json_path))
+        print(len(self.json_path))
         # TODO Change range if RAM has been upgraded
-        for i, json_file in enumerate(self.json_path[50:150]):
-            print(json_file)
+        for i, json_file in enumerate(self.json_path):
+            print(i)
             with open(json_file) as f:
                 current_dictionary = json.load(f)
             
@@ -52,12 +59,44 @@ class Andersson_dataset(Dataset):
 
             current_numpy_windows, current_numpy_one_hot_mask = self._create_sliding_window(current_annotations, current_track_id)
 
-            if self.numpy_windows is None and self.numpy_one_hot_masks is None:
-                self.numpy_windows = current_numpy_windows
-                self.numpy_one_hot_masks = current_numpy_one_hot_mask
+            if self.mode == "train":
+                if i < (len(self.json_path)/4):
+                    if self.numpy_windows is None and self.numpy_one_hot_masks is None:
+                        self.numpy_windows = [current_numpy_windows]
+                        self.numpy_one_hot_masks = [current_numpy_one_hot_mask]
+                    else:
+                        self.numpy_windows[0] = np.concatenate((self.numpy_windows[0], current_numpy_windows), axis=0)
+                        self.numpy_one_hot_masks[0] = np.concatenate((self.numpy_one_hot_masks[0], current_numpy_one_hot_mask), axis=0)
+                elif (len(self.json_path)/4) <= i < (2*len(self.json_path)/4):
+                    if len(self.numpy_windows) == 1 and len(self.numpy_one_hot_masks) == 1:
+                        self.numpy_windows.append(current_numpy_windows)
+                        self.numpy_one_hot_masks.append(current_numpy_one_hot_mask)
+                    else:
+                        self.numpy_windows[1] = np.concatenate((self.numpy_windows[1], current_numpy_windows), axis=0)
+                        self.numpy_one_hot_masks[1] = np.concatenate((self.numpy_one_hot_masks[1], current_numpy_one_hot_mask), axis=0)
+                elif (2*len(self.json_path)/4) <= i < (3*len(self.json_path)/4):
+                    if len(self.numpy_windows) == 2 and len(self.numpy_one_hot_masks) == 2:
+                        self.numpy_windows.append(current_numpy_windows)
+                        self.numpy_one_hot_masks.append(current_numpy_one_hot_mask)
+                    else:
+                        self.numpy_windows[2] = np.concatenate((self.numpy_windows[2], current_numpy_windows), axis=0)
+                        self.numpy_one_hot_masks[2] = np.concatenate((self.numpy_one_hot_masks[2], current_numpy_one_hot_mask), axis=0)
+                #elif (3*len(self.json_path)/4) <= i < (len(self.json_path)):
+                else:
+                    if len(self.numpy_windows) == 3 and len(self.numpy_one_hot_masks) == 3:
+                        self.numpy_windows.append(current_numpy_windows)
+                        self.numpy_one_hot_masks.append(current_numpy_one_hot_mask)
+                    else:
+                        self.numpy_windows[3] = np.concatenate((self.numpy_windows[3], current_numpy_windows), axis=0)
+                        self.numpy_one_hot_masks[3] = np.concatenate((self.numpy_one_hot_masks[3], current_numpy_one_hot_mask), axis=0)
+
             else:
-                self.numpy_windows = np.concatenate((self.numpy_windows, current_numpy_windows), axis=0)
-                self.numpy_one_hot_masks = np.concatenate((self.numpy_one_hot_masks, current_numpy_one_hot_mask), axis=0)
+                if self.numpy_windows is None and self.numpy_one_hot_masks is None:
+                    self.numpy_windows = current_numpy_windows
+                    self.numpy_one_hot_masks = current_numpy_one_hot_mask
+                else:
+                    self.numpy_windows = np.concatenate((self.numpy_windows, current_numpy_windows), axis=0)
+                    self.numpy_one_hot_masks = np.concatenate((self.numpy_one_hot_masks, current_numpy_one_hot_mask), axis=0)
 
             #print(self.numpy_windows.shape)
             #print(self.numpy_one_hot_masks.shape)
@@ -95,11 +134,40 @@ class Andersson_dataset(Dataset):
 
         return tensor_current_window, tensor_one_hot_mask
 
+
+    
+    def _get_length(self):
+        if self.mode == "train":
+            length = 0
+            for windows in self.numpy_windows:
+                length += len(windows)
+            return length
+        else:
+            return(len(self.numpy_windows))
+
     
 
-    def __getitem__(self, index):
-        current_numpy_window = self.numpy_windows[index]
-        current_numpy_one_hot_mask = self.numpy_one_hot_masks[index]
+    def __getitem__(self, index):        
+        if self.mode == "train":
+            if index < len(self.numpy_windows[0]):
+                #print(f"array: 0 | index: {index}")
+                current_numpy_window = self.numpy_windows[0][index]
+                current_numpy_one_hot_mask = self.numpy_one_hot_masks[0][index]
+            elif index < (len(self.numpy_windows[0]) + len(self.numpy_windows[1])):
+                #print(f"array: 1 | index: {index-len(self.numpy_windows[0])}")
+                current_numpy_window = self.numpy_windows[1][index-len(self.numpy_windows[0])]
+                current_numpy_one_hot_mask = self.numpy_one_hot_masks[1][index-len(self.numpy_one_hot_masks[0])]
+            elif index < (len(self.numpy_windows[0]) + len(self.numpy_windows[1]) + len(self.numpy_windows[2])):
+                #print(f"array: 2 | index: {index-len(self.numpy_windows[0])-len(self.numpy_windows[1])}")
+                current_numpy_window = self.numpy_windows[2][index-len(self.numpy_windows[0])-len(self.numpy_windows[1])]
+                current_numpy_one_hot_mask = self.numpy_one_hot_masks[2][index-len(self.numpy_one_hot_masks[0])-len(self.numpy_one_hot_masks[1])]
+            else:
+                #print(f"array: 3 | index: {index-len(self.numpy_one_hot_masks[0])-len(self.numpy_one_hot_masks[1])-len(self.numpy_one_hot_masks[2])}")
+                current_numpy_window = self.numpy_windows[3][index-len(self.numpy_windows[0])-len(self.numpy_windows[1])-len(self.numpy_windows[2])]
+                current_numpy_one_hot_mask = self.numpy_one_hot_masks[3][index-len(self.numpy_one_hot_masks[0])-len(self.numpy_one_hot_masks[1])-len(self.numpy_one_hot_masks[2])]
+        else:
+            current_numpy_window = self.numpy_windows[index]
+            current_numpy_one_hot_mask = self.numpy_one_hot_masks[index]
 
         tensor_current_window, tensor_one_hot_mask = self._transform(current_numpy_window, current_numpy_one_hot_mask)
         
@@ -107,11 +175,57 @@ class Andersson_dataset(Dataset):
 
     
 
-    def __len__(self): 
-        return self.numpy_windows.shape[0]
+    def __len__(self):
+        return self.length
+
 
 
 if __name__=="__main__":
-    ds = Andersson_dataset(mode="train", window_size=80)
-    print(len(ds))
+    print("\n\n")
+    print(train_json_path)
+    print(val_json_path)
+
+    '''
+    ds = Andersson_dataset(mode="train", window_size=40)
+    #print(len(ds))
+
+    print(len(ds.numpy_windows))
     
+    for windows in ds.numpy_windows:
+        print(len(windows))
+    
+    for masks in ds.numpy_one_hot_masks:
+        print(len(masks))
+
+    print(len(ds))
+
+    print("\n\n")
+    x=ds[0]
+    x=ds[61882]
+
+    print("\n\n")
+    x=ds[61883]
+    x=ds[61883+64018]
+
+    print("\n\n")
+    x=ds[61883+64019]
+    x=ds[61883+64019+62151]
+
+    print("\n\n")
+    x=ds[61883+64019+62152]
+    x=ds[61883+64019+62152+59958]
+
+    print("\n\n")
+    x=ds[61883+64019+62152+59959]
+    '''
+
+
+
+
+    
+    
+    
+
+
+
+
